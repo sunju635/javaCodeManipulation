ed3b4e49e3a631832241b73887f9e1b646a588b1
sunju <sunju635@naver.com>
2022-06-14 23:23:45 +0900

    initail commit (섹션0 업로드)

---
+++ codeManipulation/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
+    }
+}
